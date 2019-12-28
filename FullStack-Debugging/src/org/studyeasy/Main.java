5c0feb5ba0637c808e9afaef4b0c65eb4117a007
manzura <manzura@manzurasair133>
2019-12-27 22:10:01 -0600

    adding files inside project

---
+++ FullStack-Debugging/src/org/studyeasy/Main.java
@@ -0,0 +1,11 @@
+package org.studyeasy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 5, y = 0, z;
+		z = x / y;		//adding a breakpoint will stop program just before executing this line
+		System.out.println(z);
+	}
+
+}
